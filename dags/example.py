649759066f994afad8b9c6cf1421e45abc0d75cf
Vinícius Mello <viniciusdsmello@poli.ufrj.br>
2023-06-16 15:05:06 -0300

    fix(dags): adds default arguments to example dag

--- dags/example.py
+++ dags/example.py
@@ -1,6 +1,17 @@
 from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
 
-@dag
+
+default_args = {
+    'owner': 'airflow',
+}
+
+
+@dag(
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=days_ago(2), tags=['example']
+)
 def example():
 
     @task
